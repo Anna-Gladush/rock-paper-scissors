42b6aecdd6d7ff738bcc05ba1ee3043b35524e36
Anna-Gladush <ann.gladush@gmail.com>
2025-12-16 22:13:31 +0200

    Added buttons and changed some functions; getHumanChoice() became playGame(). Game is played each time the button is clicked. Added a bit of CSS

--- index.js
+++ index.js
@@ -4,56 +4,73 @@ let humanScore = 0;
 let computerScore = 0;
 let tie = 0;
 
-playGame(5);
-
 function getComputerChoice() {
     const computerChoice = Math.random();
     if (computerChoice <= 1/3) {
         console.log(`Computer's move: rock`);
         return 'rock';
-    } else if (1/3 < computerChoice <= 2/3) {
+    } else if (1/3 < computerChoice && computerChoice <= 2/3) {
         console.log(`Computer's move: paper`);
         return 'paper';
-    } else if (2/3 < computerChoice <= 1) {
+    } else if (2/3 < computerChoice && computerChoice <= 1) {
         console.log(`Computer's move: scissors`);
         return 'scissors'
     }
 }
 
-function getHumanChoice() {
-    const userInput = prompt("Pick rock/paper/scissors: ").toLowerCase();
-    console.log(`User's move: ${userInput}`);
-    return userInput;
-}
-
-
 function playRound(humanChoice, computerChoice) {
-    let result = '';
-    
+    const div = document.querySelector('.results');
+    div.innerHTML = '';
+    const p = document.createElement('p');
+    div.appendChild(p);
     if (humanChoice === computerChoice) {
         tie ++;
-        result = `It\'s a tie. 
+        p.innerText = `It\'s a tie. 
 Your score: ${humanScore}, while Computer's score: ${computerScore}. Tie's: ${tie}`;
 
     } else if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'scissors' && computerChoice === 'paper') || (humanChoice === 'paper' && computerChoice === 'rock')) {
         humanScore ++;
-        result = `You win! ${humanChoice} beats ${computerChoice}. 
+        p.innerText = `You win! ${humanChoice} beats ${computerChoice}. 
 Your score: ${humanScore}, while Computer's score: ${computerScore}. Tie's: ${tie}`;
 
     }  else if ((humanChoice === 'scissors' && computerChoice === 'rock') || (humanChoice === 'paper' && computerChoice === 'scissors') || (humanChoice === 'rock' && computerChoice === 'paper')) {
         computerScore ++;
-        result = `You lose! ${computerChoice} beats ${humanChoice}. 
+        p.innerText = `You lose! ${computerChoice} beats ${humanChoice}. 
 Your score: ${humanScore}, while Computer's score: ${computerScore}. Tie's: ${tie}`;
 
     }
-    console.log(result);
 }
 
-function playGame(num) {
-    for (let i = 0; i < num; i++) {
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection));
-    }
+function playGame() {
+    const buttons = document.querySelectorAll('.btn');
+
+    buttons.forEach((button) => {
+        let userChoice;
+
+        button.addEventListener('click', () => {
+            if (button.innerHTML === 'Rock') {
+                userChoice = 'rock';
+            } else if (button.innerHTML === 'Paper') {
+                userChoice = 'paper';
+            } else if (button.innerHTML === 'Scissors') {
+                userChoice = 'scissors';
+            }
+            const computerChoice = getComputerChoice();
+            playRound(userChoice, computerChoice);
+        });
+    })
 }
 
+
+// function playGame(num = 1) {
+//     for (let i = 0; i < num; i++) {
+//         humanSelection = getHumanChoice();
+//         computerSelection = getComputerChoice();
+//         console.log(playRound(humanSelection, computerSelection));
+//     }
+// }
+
+function gameScore() {
+
+}
+playGame();
